1f6b1519216a0512edf6eb2ae4bda3ca83bc26bb
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-12 10:20:06 +0100

    found solution

--- scramblies.js
+++ scramblies.js
@@ -1,25 +1,27 @@
 let scramble = (str1,str2) => {
 // // strings into array of strings
-let arr1 = [...str1];
-let arr2 = [...str2];
-let st1 = arr1.sort()
-console.log(st1)
-let st2 = arr2.sort()
-console.log((st2))
-let j = 0;
-let final = 0
-let check = []
-for (let i in st1) {
-  if ((check.length<st2.length) && (st2[j]===st1[i])){
-  j++;
-  final++
-  check.push(st2[i])
-  }
+// let st1 = [...str1].sort()
+// let st2 = [...str2].sort()
+// const isSubset = (array1,array2) =>
+return[...str2].sort().every((element) => [...str1].sort().includes(element));
+// return isSubset(st1,st2)
 }
-return (final===j)?true:false
- }
 
- console.log(scramble('rkqodlw', 'world'))
+// Test.assertEquals(scramble('rkqodlw','world'),true);
+// Test.assertEquals(scramble('cedewaraaossoqqyt','codewars'),true);
+// Test.assertEquals(scramble('katas','steak'),false);
+// Test.assertEquals(scramble('scriptjava','javascript'),true);
+// Test.assertEquals(scramble('scriptingjava','javascript'),true);
+// Test.assertEquals(scramble('scriptsjava','javascripts'),true);
+// Test.assertEquals(scramble('jscripts','javascript'),false);
+// Test.assertEquals(scramble('aabbcamaomsccdd','commas'),true);
+
+console.log(scramble('rkqodlw', 'world'))
 console.log(scramble('rkqodlw', 'world')) 
 console.log(scramble('cedewaraaossoqqyt', 'codewars'))
 console.log(scramble('katas', 'steak'))
+console.log(scramble('scriptjava','javascript'))
+console.log(scramble('scriptingjava','javascript'))
+console.log(scramble('scriptsjava','javascripts'))
+console.log(scramble('jscripts','javascript'))
+console.log(scramble('aabbcamaomsccdd','commas'))
